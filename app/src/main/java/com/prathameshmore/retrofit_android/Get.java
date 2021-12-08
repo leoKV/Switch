f794497fbd0f00ee33192054dbae2755765c7813
Kevin <kevorvs@gmail.com>
2021-12-07 21:28:46 -0600

    Working

---
+++ app/src/main/java/com/prathameshmore/retrofit_android/Get.java
@@ -0,0 +1,11 @@
+package com.prathameshmore.retrofit_android;
+import com.google.gson.annotations.SerializedName;
+
+public class Get {
+
+    private int status;
+
+    public int getStatus() {
+        return status;
+    }
+}
--- app/src/main/java/mx/edu/utng/switchproyect/MainActivity.java
+++ app/src/main/java/mx/edu/utng/switchproyect/MainActivity.java
@@ -1,36 +1,36 @@
 package mx.edu.utng.switchproyect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
-/*import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.NameValuePair;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.ClientProtocolException;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.entity.UrlEncodedFormEntity;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpPost;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.message.BasicNameValuePair;*/
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpRequest;
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.StatusLine;
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpPut;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.entity.StringEntity;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpGet;
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
+import com.prathameshmore.retrofit_android.Get;
 
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -38,95 +38,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void postFunction(View view){
-     new DownloadFilesTask().postFunction();
+    public interface JsonPlaceHolderApi {
+        @GET("http://192.168.1.81/switch/status.php")
+        //Call<List<Get>> getStatus();
+        Call<String> getStatus();
+    }
+
+    public void change_status(View view){
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+
+        System.out.println("Clicked");
+
+        String url = "http://192.168.1.81/switch/status.php";
+
+        StringRequest request = new StringRequest(Request.Method.GET,url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+            }},
+            new Response.ErrorListener() {
+            @Override
+                public void onErrorResponse(VolleyError Error){
+                System.out.println(Error);
+            }
+        });
+        requestQueue.add(request);
     }
 }
 
-class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
-
-    protected Long doInBackground(URL... urls) {
-// code that will run in the background
-        return null;
-    }
-
-    protected void onProgressUpdate(Integer... progress) {
-// receive progress updates from doInBackground
-    }
-
-    protected void onPostExecute(Long result) {
-// update the UI after background processes completes
-    }
-
-    public void postFunction(){
-        String result = null;
-        try {
-            // 1. create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-            // 2. make POST request to the given URL
-
-            HttpPut httpPut = new
-                    HttpPut("http://192.168.1.78:3000/switch/1");
-            String json;
-            //              // 3. build jsonObject
-            //              JSONObject jsonObject2 = new JSONObject();
-            //              jsonObject2.put("idGuarderias", idG);
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("status",1);
-            json = jsonObject.toString();
-            StringEntity se = new StringEntity(json);
-            // 6. set httpPost Entity
-            httpPut.setEntity(se);
-            // 7. Set some headers to inform server about the type of the content
-            httpPut.addHeader("Accept", "application/json");
-            httpPut.addHeader("Content-type", "application/json");
-            // 8. Execute POST request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPut);
-
-
-            //Try to add this
-            InputStream inputStream = httpResponse.getEntity().getContent();
-
-            if(inputStream != null)
-                result = "Si jaló" + inputStream.toString();
-            else
-                result = "Did not work!";
-
-            System.out.println(result);
-
-        } catch (Exception e) {
-            System.out.println("Error del catch: "+e);
-            //Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-    }
-}
-
-/*
-    public void changeSwitch(View view) {
-        HttpClient client= new DefaultHttpClient();
-        HttpPost post= new HttpPost("http://localhost:3000/peliculas/reAp");
-        try {
-            List<NameValuePair> nameValuePairs= new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id","reAp"));
-            nameValuePairs.add(new BasicNameValuePair("titulo","Resident Evil Apocalipsis"));
-            nameValuePairs.add(new BasicNameValuePair("director","Alexander Witt"));
-            nameValuePairs.add(new BasicNameValuePair("productora","Sony"));
-            nameValuePairs.add(new BasicNameValuePair("anio_salida","2004"));
-            nameValuePairs.add(new BasicNameValuePair("character_main","Alice Abernathy"));
-            nameValuePairs.add(new BasicNameValuePair("sinopsis","Prueba 1"));
-            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = client.execute(post);
-        }catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ;
-
-
-    }
-}*/
